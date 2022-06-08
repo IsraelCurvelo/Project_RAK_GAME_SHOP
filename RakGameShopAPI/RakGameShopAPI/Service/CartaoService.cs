afe1c7ee3ffe2f9cb82a8ef76fafd6e51af585f2
Israel Curvelo <israel.curvelo@bizapp.com.br>
2022-06-07 23:59:49 -0300

    Ajustes Referências

--- RakGameShopAPI/RakGameShopAPI/Service/CartaoService.cs
+++ RakGameShopAPI/RakGameShopAPI/Service/CartaoService.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Crm.Sdk.Messages;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
-using Microsoft.Xrm.Tooling.Connector;
 
 namespace RakGameShopAPI.Service
 {
